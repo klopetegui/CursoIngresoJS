72d0af8a02623f9e3d314fb1f3c7570ca6ee20a6
klopetegui <karenfernandezlopetegui@gmail.com>
2021-02-08 18:36:46 -0300

    Ejercicio dos parcial 2019 ok

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
-  
+  var nombreUno;
+  var nombreDos;
+  var pesoUno;
+  var pesoDos;
+  var suma;
+  var promedio;
+
+nombreUno= prompt("Ingresar el nombre del primer integrante de la pareja");
+
+
+nombreDos= prompt("Ingresar el nombre del segundo integrante de la pareja");
+
+
+pesoUno= prompt("Ingresar el peso del primer integrante de la pareja");
+pesoUno= parseInt(pesoUno);
+
+
+pesoDos= prompt("Ingresar el peso del segundo integrante de la pareja");
+pesoDos= parseInt(pesoDos);
+
+suma= pesoUno+pesoDos;
+
+promedio= suma/2;
+
+
+
+alert("Ustedes se llaman "+nombreUno+ " y "+ nombreDos+", pesan "+pesoUno+ " y "+pesoDos+ "kilos y el promedio de peso es de "+promedio+ " kilos.");
+
+
+
 }
