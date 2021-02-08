0207fc478caf96ab8ec845f229c33c06076218cd
klopetegui <karenfernandezlopetegui@gmail.com>
2021-02-08 17:28:05 -0300

    ejercicio del parcial 2018 uno resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var perimetro;
+
+ancho= prompt("ingrese ancho del rectangulo");
+ancho= parseInt(ancho);
+
+largo= prompt("Ingrese largo del rectangulo");
+largo= parseInt(largo);
+
+perimetro= 2*(ancho+largo);
+
+alert("El perimetro de su rectangulo es: "+perimetro);
+
+
+
+
 
 }
