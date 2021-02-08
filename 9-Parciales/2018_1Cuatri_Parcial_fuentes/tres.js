76b1331d22fa1d058aca4942157a5c2fcf49c136
klopetegui <karenfernandezlopetegui@gmail.com>
2021-02-08 17:56:25 -0300

    resolucion del ejercicio tres

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+ var precio;
+ var porcentaje;
+ var conversionPorcentaje;
+ var total;
+
+precio=prompt("Ingrese precio");
+precio= parseInt(precio);
+
+porcentaje= prompt("Ingrese porcentaje de descuento"); 
+porcentaje= parseInt(porcentaje);
+
+conversionPorcentaje=precio*(porcentaje/100);
+
+total= precio - conversionPorcentaje;
+
+elPrecioFinal.value= total;
+
+
+
 
 }
