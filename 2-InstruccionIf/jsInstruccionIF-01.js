f8755be72c0c739f06bf9249bfd03233977a723d
klopetegui <karenfernandezlopetegui@gmail.com>
2021-02-08 19:21:06 -0300

    ejercicio 1 if

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	
+	var edad;
+
+	edad= txtIdEdad.value;
+	edad= parseInt(edad);
+
+	if(edad ==15)
+{
+	alert("Niña bonita");
+	
+
+}
 
 }//FIN DE LA FUNCIÓN
