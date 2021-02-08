229b8190cf9f358d2b3463b78bb2c0d63a8d0cb5
klopetegui <karenfernandezlopetegui@gmail.com>
2021-02-08 18:07:21 -0300

    ejercicio uno del segundo parcial ok

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+var ladoDelTriangulo;
+var perimetro;
+
+
+ladoDelTriangulo= prompt("Por favor,ingrese la medida de  uno de los lados del triangulo equilatero");
+ladoDelTriangulo=parseInt(ladoDelTriangulo);
+
+perimetro= 3* ladoDelTriangulo;
+
+alert("El perimetro de su triangulo equilatero es: "+perimetro);
+
+
 
 }
